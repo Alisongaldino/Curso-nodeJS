856f25db75bea2f702ba2b60f9a2846ec642767a
Alisongaldino <alisongaldino19@gmail.com>
2024-07-02 22:00:02 -0300

    Salvando usuários em memoria

--- fundamentosnodejs/src/server.js
+++ fundamentosnodejs/src/server.js
@@ -10,17 +10,27 @@ import http from 'node:http'
 //GET /users => Buscar usuario
 // POST / users => criar usuario
 
+const users = []
+
 const server = http.createServer((req,res) => {
     const { method, url } = req
 
-    if (method == 'GEt' && url == '/users') {
-        return res.end ('listagem de users')
+    if (method == 'GET' && url == '/users') {
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end (JSON.stringify(users))
     }
   
-    if (method == 'POST' & url == '/user') {
+    if (method == 'POST' && url == '/users') {
+        users.push({
+            id: 1,
+            name:'nara',
+            email: 'naralinda@gmail.com',
+        })
         return res.end ('Criação de users')
     }
 
+    return res.end ('a')
 })
 
-server.listen(5050)
+server.listen(3333)

eaca262b6d0dd4d1b75937bf5dc6e50208ab5e99
Alisongaldino <alisongaldino19@gmail.com>
2024-07-02 21:30:42 -0300

    Metodos http

--- fundamentosnodejs/src/server.js
+++ fundamentosnodejs/src/server.js
@@ -1,10 +1,26 @@
 import http from 'node:http'
 
+//HTTP
+// GET => buscar
+// POST => criar
+// PUT => atualizar
+// PATCH => atualizção especifica
+// DELETE => deletar
+
+//GET /users => Buscar usuario
+// POST / users => criar usuario
 
 const server = http.createServer((req,res) => {
-return res.end('ola mundo')
+    const { method, url } = req
+
+    if (method == 'GEt' && url == '/users') {
+        return res.end ('listagem de users')
+    }
+  
+    if (method == 'POST' & url == '/user') {
+        return res.end ('Criação de users')
+    }
+
 })
 
 server.listen(5050)
-
-//localhost:3333

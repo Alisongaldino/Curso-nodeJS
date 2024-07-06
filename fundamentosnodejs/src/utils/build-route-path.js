c77d679ce162a833a75485c3e179883bbcc6e182
Alisongaldino <alisongaldino19@gmail.com>
2024-07-06 13:24:07 -0300

    Remoção de registros

--- fundamentosnodejs/src/utils/build-route-path.js
+++ fundamentosnodejs/src/utils/build-route-path.js
@@ -1,7 +1,7 @@
 // /users/ID'
 export function buildRoutePath(path) {
     const routeParametesRegex = /:([a-zA-Z]+)/g
-    const pathWithParms =path.replaceAll(routeParametesRegex, '(?<$1>[a-z0-9\-_]+)')
+    const pathWithParms = path.replaceAll(routeParametesRegex, '(?<$1>[a-z0-9\-_]+)')
 
 
     const pathRegex =new RegExp(`^${pathWithParms}`)
